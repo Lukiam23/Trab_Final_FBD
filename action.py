78cc01a2c0b6f34e04f3a985aefd6f6b9fb99fd2
Lukiam23 <mscorreia95@gmail.com>
2018-11-29 21:12:05 +0000

    Atualizações

--- action.py
+++ action.py
@@ -8,6 +8,8 @@ from kivy.uix.scrollview import ScrollView
 from kivy.config import Config
 from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
+from functools import partial
+from kivy.clock import Clock
 
 Config.set('graphics', 'width', '1380')
 Config.set('graphics', 'height', '715')
@@ -19,18 +21,37 @@ Config.set('kivy','window_icon','C:\\Users\\mscor\\Documents\\2018.2\\FBD\\logo.
 class Menu(Screen):
 	def __init__(self,**kwargs):
 		super(Menu,self).__init__(**kwargs)
+	def on_enter(self):
+		Clock.schedule_once(self.listAllMusics)
+	def listAllPlaylist(self,*arg):
+		playlistscreen = self.manager.get_screen(name='playlist')
+		playlistscreen.populate()
+	def listAllAlbums(self,*arg):
+		albumscreen = self.manager.get_screen(name='albums')	
+		albumscreen.populate()
+	def listAllMusics(self,*arg):
+		musicscreen = self.manager.get_screen(name='music')	
+		musicscreen.populate()
 
 
 class GenericScreen(Menu):
 	def __init__(self,**kwargs):
-		super(GenericScreen,self).__init__(**kwargs)
-		
+		super(GenericScreen,self).__init__(**kwargs)		
 
 
 class MusicScreen(Menu):
 	
 	def __init__(self,**kwargs):
 		super(MusicScreen,self).__init__(**kwargs)
+
+	def populate(self):	
+		self.ids.box.clear_widgets()
+		tarefas = ["Four seasons","Virus","Trino Del Diablo" ]	
+		for tarefa in tarefas:
+			playlist = Button(text=tarefa,font_size=20,size_hint_y=None,height=50,background_color=(0.1,0.1,0.1,1))
+			self.ids.box.add_widget(playlist)
+		playlists = ["Classica","Barroca","Moderna" ]	
+		
 		
 		
 class PlayListScreen(Menu):
@@ -40,29 +61,46 @@ class PlayListScreen(Menu):
 		
 	def populate(self):	
 		self.ids.box.clear_widgets()
-		tarefas = [str(x) for x in range(1,100)]	
+		tarefas = ["Classica","Barroca","Moderna" ]	
 		for tarefa in tarefas:
-			self.ids.box.add_widget(Label(text=tarefa,font_size=30,size_hint_y=None,height=200))
+			playlist = Button(text=tarefa,font_size=20,size_hint_y=None,height=50,background_color=(0.1,0.1,0.1,1))
+			self.ids.box.add_widget(playlist)
 
 class AlbumsScreen(Menu):
 	box = ObjectProperty(None)
 	def __init__(self,**kwargs):
 		super(AlbumsScreen,self).__init__(**kwargs)
 		self.albumsData = {}
-	def musics(self,album):
-		self.manager.current = "generic"
+
+	def search(self):
+		self.description  = self.ids.textsearch.text
+		print(self.description)
+	def musics(self,album,*args):
+		
+		generic = self.manager.get_screen(name='generic')
+		self.manager.current = 'generic'
+		generic.ids.title.text = album
+		generic.ids.box.clear_widgets()	
+		for i in range(1,3):
+			btn = Label(text="Musica do Album",font_size=20,size_hint_y=None,height=50,text_size= self.size,halign='left',valign='middle',pos_hint={'top': 1,'x':0.18})
+
+			generic.ids.box.add_widget(btn)
+
 	def populate(self):	
-		self.ids.box.clear_widgets()
-		tarefas = [str(x) for x in range(1,100)]	
-		for tarefa in tarefas:
-			original = "Florence and the Machine: Between Two Lungs"
-			if(len(original)>50):
-				text = original.split()
-				text = " ".join(text[0:5]) + "..."
-			else:
-				text=original
-			btn = Button(text=text,font_size=20,size_hint_y=None,height=100,background_color=(0.1,0.1,0.1,1),on_press=lambda a:self.musics(original))
+		self.ids.box.clear_widgets()	
+		self.albums = ["Florence and the Machine: Between Two Lungs","Bethoven","Niccolo Paganini"]
+		i = 0
+		for album in self.albums:
+			titulo = album
+			if(len(titulo)>60):
+				titulo = titulo.split()
+				titulo = " ".join(titulo[0:5]) + "..."
+
+			btn = Button(text=titulo,font_size=20,size_hint_y=None,height=100,background_color=(0.1,0.1,0.1,1))
+			btn.bind(on_press=partial(self.musics,album))
 			self.ids.box.add_widget(btn)
+
+
 
 
 class RecorderScreen(Menu):
@@ -83,11 +121,8 @@ class Manager(ScreenManager):
 	compositorScreen = ObjectProperty(None)
 	musicalPeriod = ObjectProperty(None)
 
-class Teste(BoxLayout):
-	pass
 
 class ActionApp(App):
-
 	def build(self):
 		self.title = "SpotPer"
 		return Manager()
