3b228b119962f955b822f2d099a3150acb6a4bda
Franklyn-S <franklyseabra@alu.ufc.br>
2018-12-05 22:22:34 -0300

    Corrigindo erros

--- Consultas.sql
+++ Consultas.sql
@@ -3,20 +3,20 @@ use spotper;
 --9a)
 select * from album where preco > (select avg(preco) media from album);
 --9b) 
-select g.nome, count(fp.cod_play)
+select g.nome_grav, count(fp.cod_playlist) 'qtd playlist'
 from gravadora g inner join album a on g.cod_grav=a.cod_grav
 	 inner join faixa f on a.cod_album = f.cod_album
-	 inner join faixa_playlist fp on fp.cod_album = f.cod_album and fp.numero_faixa=f.numero_faixa
-	 inner join faixa_compositor fc on f.numero_faixa = fc.numero_faixa and f.cod_album = fc.cod_album
-	 inner join compositor c on fc.id_comp = c.id_comp
+	 inner join faixa_playlist fp on fp.cod_album = f.cod_album and fp.numero=f.numero
+	 inner join faixa_compositor fc on f.numero = fc.numero and f.cod_album = fc.cod_album
+	 inner join compositor c on fc.cod_compositor = c.cod_compositor
 	 and c.nome='Dvorack'
-group by g.cod_grav, g.nome
-having COUNT(fp.cod_play)>=all(select count(fp.cod_play)
+group by g.cod_grav, g.nome_grav
+having COUNT(fp.cod_playlist)>=all(select count(fp.cod_playlist)
 							from gravadora g inner join album a on g.cod_grav=a.cod_grav
 							inner join faixa f on a.cod_album = f.cod_album
-							inner join faixa_playlist fp on fp.cod_album = f.cod_album and fp.numero_faixa=f.numero_faixa
-							inner join faixa_compositor fc on f.numero_faixa = fc.numero_faixa and f.cod_album = fc.cod_album
-							inner join compositor c on fc.id_comp = c.id_comp
+							inner join faixa_playlist fp on fp.cod_album = f.cod_album and fp.numero=f.numero
+							inner join faixa_compositor fc on f.numero = fc.numero and f.cod_album = fc.cod_album
+							inner join compositor c on fc.cod_compositor = c.cod_compositor
 							and c.nome='Dvorack'
 							group by g.cod_grav)
 
